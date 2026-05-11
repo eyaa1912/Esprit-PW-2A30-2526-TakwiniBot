955702c09acee320c34b7626ec31cd3bf099791a
oumayma-dhahri <Oumayma.Dhahri@esprit.tn>
2026-05-11 23:25:38 +0100

    Delete gestion_formation directory

--- gestion_formation/View/backoffice-formation/deleteInscription.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../Controller/Inscriptioncontroller .php';
-
-$ic = new InscriptionController();
-$ic->deleteInscription((int)$_GET['id']);
-
-header('Location: listInscriptions.php');
-exit;
-?>
