26b7478fd20b7a62fbf8ba20e283320c4844516b
oumayma-dhahri <Oumayma.Dhahri@esprit.tn>
2026-05-12 15:25:36 +0100

    Delete api_mistral.php

--- api_mistral.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Clé API Mistral — ne pas partager publiquement
-define('MISTRAL_API_KEY', 'wh7vsCYd8w8I8v1lOUzQByTdIEaYdaAS');
-define('MISTRAL_API_URL', 'https://api.mistral.ai/v1/chat/completions');
-define('MISTRAL_MODEL',   'mistral-small-latest');
