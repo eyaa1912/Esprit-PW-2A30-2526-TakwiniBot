f082d222e1af15b164970fc1502970e5b3b278c2
oumayma-dhahri <Oumayma.Dhahri@esprit.tn>
2026-05-12 15:24:05 +0100

    Delete View directory

--- View/Admin/pages/includes/scripts.php
+++
@@ -1,3 +0,0 @@
-<script src="../assets/vendor/libs/jquery/jquery.js"></script>
-<script src="../assets/vendor/js/bootstrap.js"></script>
-<script src="../assets/js/main.js"></script>
--- View/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="text-center py-4 bg-light mt-5">
-    <p>© <?= date('Y') ?> Takwinibot</p>
-</footer>
