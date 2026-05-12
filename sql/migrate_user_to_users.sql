ac4982c5ceb8720f3c3b59bb2349b9c7c822a9ef
oumayma-dhahri <Oumayma.Dhahri@esprit.tn>
2026-05-12 15:25:09 +0100

    Delete sql directory

--- sql/migrate_user_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Renommer la table user en users et ajouter le champ prenom
-RENAME TABLE `user` TO `users`;
-
-ALTER TABLE `users`
-  ADD COLUMN `prenom` varchar(100) NOT NULL DEFAULT '' AFTER `nom`;
